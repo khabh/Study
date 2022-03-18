a95b802bd7537659f36de9e48bc5f9b9b250e3b5
JUHA <84626225+khabh@users.noreply.github.com>
2022-03-18 23:26:30 +0900

    Create scannerex.java

---
+++ Java/scannerex.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ScannerEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.printf("Enter two digits integer.>");
+		String input = scanner.nextLine();
+		int num=I34nteger.parseInt(input);
+		
+		System.out.println("your input:"+input);
+		
+		
+		
+		
+	}
+
+}
