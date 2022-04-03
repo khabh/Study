60642fe109210ac6df45f648178cc0df1da2512b
khabh <khabh@naver.com>
2022-04-03 16:10:31 +0900

    recursive call ex

---
+++ Java/ch6/FactorialTest.java
@@ -0,0 +1,19 @@
+class FactorialTest {
+    public static void main(String[] args) {
+        int result = factorial(4 );
+        System.out.println(result);
+    }
+
+    static int factorial(int n) {
+        int result=0;
+        if(n<0||n>12) return -1; //매개변수의 유효성 검사
+        if (n==1) 
+            result=1;
+        else
+            result=n*factorial(n-1);
+        
+        return result;
+        
+    }
+
+}
