a1c1b759283ec5ee41ee3ecea342e2278eb70beb
JUHA <khabh@naver.com>
2022-04-04 23:37:41 +0900

    문서 테스트

---
+++ Java/ch6/BlockTest.java
@@ -0,0 +1,23 @@
+class BlockTest {
+    static {
+        System.out.println("static { }");
+    }//클래스 초기화 블럭이 가장 먼저 실행됨
+    {
+        System.out.println("{ }");
+    }// 인스턴스가 생성된 후 즉시 실행됨
+    public BlockTest() {
+        System.out.println("생성자");
+    }
+    
+
+
+
+    public static void main(String[] args) {
+        System.out.println("BlockTest bt = new BlockTest(); ");
+        BlockTest bt = new BlockTest();
+
+        System.out.println("BlockTest bt2 = new BlockTest(); ");
+        BlockTest bt2 = new BlockTest();
+    }
+
+}
