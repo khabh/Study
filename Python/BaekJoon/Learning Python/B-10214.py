8c6786e221eaee14ceeef197e620b422762a7cea
JUHA <khabh@naver.com>
2022-04-09 10:07:51 +0900

    Python

---
+++ Python/BaekJoon/Learning Python/B-10214.py
@@ -0,0 +1,15 @@
+case = int(input())
+
+for i in range(case):
+    a = b = 0
+    for _ in range(9):
+        x, y = map(int, input().split())
+        a += x
+        b += y
+
+    if a > b:
+        print("Yonsei")
+    elif a < b:
+        print("Korea")
+    else:
+        print("Draw")
