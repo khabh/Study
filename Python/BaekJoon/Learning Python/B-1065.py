de1a084ea0941b9599cc2fb6ab8bf7f7616405f1
JUHA <khabh@naver.com>
2022-04-12 14:15:04 +0900

    22/04/12 python

--- Python/BaekJoon/Learning Python/B-1065.py
+++ Python/BaekJoon/Learning Python/B-1065.py
@@ -1,18 +1,19 @@
 def find(n):
-    numbers=list(str(n))
-    if len(numbers)<=2:
+    if n < 100:
         return True
-    for i in range (len(numbers)-2):
-        if int(numbers[i+1])-int(numbers[i])!=int(numbers[i+2])-int(numbers[i+1]):
+    numbers = list(str(n))
+
+    for i in range(len(numbers)-2):
+        if int(numbers[i+1])-int(numbers[i]) != int(numbers[i+2])-int(numbers[i+1]):
             return False
-            
+
     return True
-        
-        
-N=int(input())
-cnt=0
-for i in range (1,N+1):
-    if find(i)==True:
-        cnt+=1
-        
+
+
+N = int(input())
+cnt = 0
+for i in range(1, N+1):
+    if find(i) == True:
+        cnt += 1
+
 print(cnt)
