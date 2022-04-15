d3a8fe70380e37a49116c9e4101afccd3ae51b07
JUHA <khabh@naver.com>
2022-04-15 09:30:26 +0900

    22/04/15 python

--- Python/BaekJoon/Hi, so to me seems like a notorious coincidence/1890.py
+++ Python/BaekJoon/Hi, so to me seems like a notorious coincidence/1890.py
@@ -1,10 +1,41 @@
 num = int(input())
 array = []
+move = [[0, 0]]
+x = y = cnt = 0
 for _ in range(num):
     array.append(list(map(int, input().split())))
 
 
-def moveRight(x, y):
+def moveLeft(x1, y1):
     global array
-    if (x+array[x][y]) <= 3:
-        dx = x+array[x][y]
+    global move
+    global cnt
+    if (x1+array[x1][y1]) <= 3:
+        if x1+array[x1][y1] == 3 and y1 == 3:
+            cnt += 1
+        else:
+            move.append([x1+array[x1][y1], y1])
+
+
+def moveDown(x1, y1):
+    global array
+    global move
+    global cnt
+    if (y1+array[x1][y1]) <= 3:
+        if (y1+array[x1][y1]) == 3 and x1 == 3:
+            cnt += 1
+        else:
+            move.append([x1, y1+array[x1][y1]])
+
+
+i = 0
+
+while True:
+    moveLeft(move[i][0], move[i][1])
+    moveDown(move[i][0], move[i][1])
+    if i+1 <= len(move)-1:
+        i += 1
+    else:
+        break
+
+print(cnt)
