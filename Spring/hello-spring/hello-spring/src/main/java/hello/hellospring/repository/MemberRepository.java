055d1f259bf9b5e042d5d7879c34db14ee9e82ea
JUHA <khabh@naver.com>
2022-04-30 18:04:21 +0900

    22/04/30 spring

---
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.controller.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findId(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
