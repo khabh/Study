67a17343892956493df17b0e3e2e9651438d66aa
JUHA <khabh@naver.com>
2022-05-07 20:10:34 +0900

    22/05/07 java

--- Java/codingtest/Main.java
+++ Java/codingtest/Main.java
@@ -1,24 +1,24 @@
-import java.util.*;
+// import java.util.*;
 
-class Main {
-    public int solution(String str, char t) {
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-        for (int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == t) answer++;
-        }
-        return answer;
-    }
+// class Main {
+//     public int solution(String str, char t) {
+//         int answer = 0;
+//         str = str.toUpperCase();
+//         t = Character.toUpperCase(t);
+//         for (int i = 0; i < str.length(); i++){
+//             if(str.charAt(i) == t) answer++;
+//         }
+//         return answer;
+//     }
 
-    public static void main (String[] args) {
-        Main T = new Main();
+//     public static void main (String[] args) {
+//         Main T = new Main();
     
-        Scanner kb = new Scanner(System.in); //Scanner 생성
-        String str = kb.next();
-        char c = kb.next().charAt(0);
-        System.out.print(T.solution(str,c));
-    }
+//         Scanner kb = new Scanner(System.in); //Scanner 생성
+//         String str = kb.next();
+//         char c = kb.next().charAt(0);
+//         System.out.print(T.solution(str,c));
+//     }
 
     
-}
+// }
