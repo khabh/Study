88532dcab8dd0a014424c36d52303af4292b62ec
JUHA <khabh@naver.com>
2022-05-11 10:44:13 +0900

    22/05/11 java

--- Java/ch7/InterfaceTest2.java
+++ Java/ch7/InterfaceTest2.java
@@ -1,32 +1,32 @@
-interface I {
-    public abstract void play();
-}
+// interface I {
+//     public abstract void play();
+// }
 
-class B implements I {
-    public void play() {
-        System.out.println("play in a B class.");
+// class B implements I {
+//     public void play() {
+//         System.out.println("play in a B class.");
 
-    }
-}
+//     }
+// }
 
-class C implements I {
-    public void play() {
-        System.out.println("play in a C class.");
-    }
-}
+// class C implements I {
+//     public void play() {
+//         System.out.println("play in a C class.");
+//     }
+// }
 
-class A {
-    void autoplay(I i) {
-        i.play();
-    }
-}
+// class A {
+//     void autoplay(I i) {
+//         i.play();
+//     }
+// }
 
 
-class InterfaceTest2 {
-    public static void main(String[] args) {
-        // Enter Your Code Here
-        A a = new A();
-        a.autoplay(new B());
-        a.autoplay(new C());
-    }
-}
+// class InterfaceTest2 {
+//     public static void main(String[] args) {
+//         // Enter Your Code Here
+//         A a = new A();
+//         a.autoplay(new B());
+//         a.autoplay(new C());
+//     }
+// }
