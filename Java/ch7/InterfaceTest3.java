110bff4a25160a656e5f04307a23446fcd2607eb
JUHA <khabh@naver.com>
2022-05-13 09:06:19 +0900

    22/05/13 java

--- Java/ch7/InterfaceTest3.java
+++ Java/ch7/InterfaceTest3.java
@@ -1,35 +1,35 @@
-class InterfaceTest3 {
-    public static void main(String[] args) {
-        // Enter Your Code Here
-        A a = new A();
-        a.methodA();
-    }
-}
+// class InterfaceTest3 {
+//     public static void main(String[] args) {
+//         // Enter Your Code Here
+//         A a = new A();
+//         a.methodA();
+//     }
+// }
 
-interface I {
-    public abstract void methodB();
-}
+// interface I {
+//     public abstract void methodB();
+// }
 
-class B implements I {
-    public void methodB() {
-        System.out.println("methodB in B class.");
-    }
+// class B implements I {
+//     public void methodB() {
+//         System.out.println("methodB in B class.");
+//     }
 
-    public String toString() {
-        return "class B";
-    }
-}
+//     public String toString() {
+//         return "class B";
+//     }
+// }
 
-class InstanceManager {
-    public static I getInstance() {
-        return new B();
-    }
-}
+// class InstanceManager {
+//     public static I getInstance() {
+//         return new B();
+//     }
+// }
 
-class A {
-    void methodA() {
-        I i = InstanceManager.getInstance();
-        i.methodB();
-        System.out.println(i.toString());
-    }
-}
+// class A {
+//     void methodA() {
+//         I i = InstanceManager.getInstance();
+//         i.methodB();
+//         System.out.println(i.toString());
+//     }
+// }
