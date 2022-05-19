980520717be96cdffbc0f8c8f8f9b6410db13a3c
JUHA <khabh@naver.com>
2022-05-19 16:49:18 +0900

    22/05/19 python

---
+++ Python/coding test/그래프이론/10-7.py
@@ -0,0 +1,30 @@
+
+
+def find_parent(student, x):
+    if student[x] != x:
+        student[x] = find_parent(student, student[x])
+    return student[x]
+
+
+def union(student, a, b):
+    a = find_parent(student, a)
+    b = find_parent(student, b)
+    if a < b:
+        student[b] = a
+    else:
+        student[a] = b
+
+
+n, m = map(int, input().split())
+student = [0]*(n+1)
+for i in range(1, n+1):
+    student[i] = i
+for _ in range(m):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union(student, a, b)
+    else:
+        if find_parent(student, a) == find_parent(student, b):
+            print("YES")
+        else:
+            print("NO")
