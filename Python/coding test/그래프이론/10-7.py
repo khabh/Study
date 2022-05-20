024dbe26a0d2a67e402d541eece57f5a6c63e0b5
JUHA <khabh@naver.com>
2022-05-20 21:37:43 +0900

    22/05/20

--- Python/coding test/그래프이론/10-7.py
+++ Python/coding test/그래프이론/10-7.py
@@ -17,8 +17,10 @@ def union(student, a, b):
 
 n, m = map(int, input().split())
 student = [0]*(n+1)
+
 for i in range(1, n+1):
     student[i] = i
+
 for _ in range(m):
     op, a, b = map(int, input().split())
     if op == 0:
