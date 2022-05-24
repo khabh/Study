89a53a39ee0bbe46059a10bed93b28a7ff8af183
JUHA <khabh@naver.com>
2022-05-24 15:44:01 +0900

    22/05/24 python

--- Python/coding test/test/다익스트라알고리즘.py
+++ Python/coding test/test/다익스트라알고리즘.py
@@ -1,25 +1,44 @@
-import heapq
 import sys
-
 input = sys.stdin.readline
 INF = int(1e9)
 
 n, m = map(int, input().split())
 start = int(input())
-graph = [[] for i in range(n+1)]
+
+graph = [[] for _ in range(n+1)]
 distance = [INF] * (n+1)
+visited = [False] * (n+1)
 
 
-def find(start):
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def smallest_dis():
+    smallest = INF
+    index = 0
+    for i in range(1, n+1):
+        if not visited[i] and distnace[i] < smallest:
+            smallest = distance[i]
+            index = i
+    return index
+
+
+def dijkstra(start):
     distance[start] = 0
-    q = []
-    heapq.heappush(q, (0, start))  # (거리, 노드)
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+    visited[start] = True
+    for i in graph[start]:
+        distance[i[0]] = i[1]
+    for _ in range(n-1):
+        now = smallest_dis()
+        visited[now] = True
+        for j in graph[now]:
+            cost = j[1] + distance[now]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start)
+for i in range(1, n+1):
+    print(distance[i])
