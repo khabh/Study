c52185d82eca96963fc2485a27f381944115d5c2
JUHA <khabh@naver.com>
2022-05-25 13:08:56 +0900

    22/05/25 python

--- Python/coding test/test/사이클판별.py
+++ Python/coding test/test/사이클판별.py
@@ -1,3 +1,7 @@
+import sys
+input = sys.stdin.readline
+
+
 def find_parent(x, parent):
     if parent[x] != x:
         parent[x] = find_parent(parent[x], parent)
@@ -14,14 +18,17 @@ def union(a, b, parent):
         parent[a] = b
 
 
-n, m = map(int, input().split())
+n, m = map(int, input().split())  # 노드 와 간선 개수
+cycle = False
 parent = [i for i in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    union(a, b, parent)
+    if find_parent(a, parent) == find_parent(b, parent):
+        cycle = True
+    else:
+        union(a, b, parent)
 
-for i in range(1, n+1):
-    print(find_parent(i, parent), end=' ')
-
-print()
-print(*parent)
+if cycle:
+    print('사이클이 발생했습니다.')
+else:
+    print('사이클이 발생하지 않았습니다.')
