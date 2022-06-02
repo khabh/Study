6e32b32fedc7c5c9ed3477fe47bcbee24a4be540
JUHA <khabh@naver.com>
2022-06-02 17:21:29 +0900

    22/06/02 python

---
+++ Python/BaekJoon/greedy/15904.py
@@ -0,0 +1,5 @@
+n = input()
+if 'U' in n and n.count('C') >= 2 and 'P' in n:
+    print('I love UCPC')
+else:
+    print('I hate UCPC')
---
+++ Python/BaekJoon/greedy/16435.py
@@ -0,0 +1,11 @@
+n, l = map(int, input().split())
+fruits = list(map(int, input().split()))
+fruits.sort()
+
+for fruit in fruits:
+    if l >= fruit:
+        l += 1
+    else:
+        break
+
+print(l)
