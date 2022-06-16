776054e55123a38c48a010ab64c2b1df044d171c
JUHA <khabh@naver.com>
2022-06-16 22:59:07 +0900

    22/06/16 python

--- Python/BaekJoon/greedy/11501.py
+++ Python/BaekJoon/greedy/11501.py
@@ -1,20 +1,55 @@
-import sys
-input = sys.stdin.readline
+print([1,2,3,5,4].split(max[1,2,3,5,4]))
 
-t = int(input())
-for _ in range (t):
-    result = 0
-    n = int(input())
-    stock = list(map(int,input().split()))
-    while True:
-        if len(stock) > 0:
-          maxValue = max(stock)
-        else:
-          break
-        maxIndex = stock.index(maxValue)
-        result += maxValue * maxIndex - sum(stock[:maxIndex])
-        if maxIndex == n-1:
-            break
-        stock = stock[maxIndex+1:]
+
+# import sys
+# input = sys.stdin.readline
+
+# case = int(input())
+# for _ in range(case):
+#     result = 0
+#     n = int(input())
+#     stock = list(map(int,input().split()))
+#     if sorted(stock, reverse = True) == stock:
+#           print(0)
+#           continue
+#     while stock:
+#           maxPrice = max(stock)
+#           for i in range (0,len(stock)):
+#                 if stock[i] == maxPrice:
+#                       result += maxPrice * i - sum(stock[:i])
+#                       break
+#           if 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import sys
+# # input = sys.stdin.readline
+
+# # t = int(input())
+# # for _ in range (t):
+# #     result = 0
+# #     n = int(input())
+# #     stock = list(map(int,input().split()))
+# #     while True:
+# #         if len(stock) > 0:
+# #           maxValue = max(stock)
+# #         else:
+# #           break
+# #         maxIndex = stock.index(maxValue)
+# #         result += maxValue * maxIndex - sum(stock[:maxIndex])
+# #         if maxIndex == n-1:
+# #             break
+# #         stock = stock[maxIndex+1:]
         
-    print(result)       
+# #     print(result)       
+
