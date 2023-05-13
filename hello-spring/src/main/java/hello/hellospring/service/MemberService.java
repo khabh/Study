5c07f7d2d3c8e14833e62ae2b7927b9648e29e65
JUHA <khabh@naver.com>
2023-05-13 21:37:47 +0900

    refactor(Member): 컴포넌트 스캔을 이용한 의존성 주입 예제

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,17 +5,21 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     /**
-    * 회원 가입
+     * 회원 가입
      */
     public Long join(Member member) {
         validateDuplicateMember(member);
