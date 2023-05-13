9f5afcf2fb3adfa7bc8d2e9c905dc1728697265f
JUHA <khabh@naver.com>
2023-05-13 21:51:45 +0900

    refactor(Member): Configuration을 이용해서 직접 Bean 객체 등록

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private final static Map<Long, Member> store = new HashMap<>();
